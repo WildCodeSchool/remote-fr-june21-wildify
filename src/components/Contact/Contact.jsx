1263c6d77d7f77358e4ee10535ed0340462fecd9
Guillaume Lequin <guillaume.lequin713@gmail.com>
2021-07-23 11:05:15 +0200

    delete useless dependencies

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,26 +4,7 @@ import React, { useState } from "react";
 import './Contact.css';
 
 const Contact = () => {
-  const [status, setStatus] = useState("Submit");
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setStatus("Sending...");
-    const { name, email, message } = e.target.elements;
-    let details = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    };
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(details),
-    });
-    setStatus("Submit");
-    let result = await response.json();
-    alert(result.status);
+  
   };
   return (
     <div className="form-global">
