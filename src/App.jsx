e778d0713ef7a996ad557c8740289f4ed165c7e1
Axel Gourlin <gourlin.axel@gmail.com>
2021-07-23 11:51:10 +0200

    modif quote and position import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 import Explore from './Components/Explore/Explore.jsx'
--- src/Components/Explore/Explore.jsx
+++ src/Components/Explore/Explore.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+
 import ExploreArtistCard from './ExploreArtistCard'
 
 import './Explore.css';
