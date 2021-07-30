43fefba09305e3c93652a05a17d9a9d24a25a28e
Axel Gourlin <gourlin.axel@gmail.com>
2021-07-30 09:56:24 +0200

    rename Components => component

---
+++ src/component/Legal.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Legal.css';
+
+const Legal = () => (
+    <div>
+        <section className="bloc-infos">
+            <h2>Mentions Légales</h2>
+            <article className="bloc-text">
+                <h3>Identité</h3>
+                <p>Wildify</p>
+                <p>Lieu-dit des concerts</p>
+                <p>28240 La Loupe</p>
+                <p>Siret : 000 000 000 000000 - RCS La Loupe</p>
+            </article>
+            <article className="bloc-text">
+                <h3>Hébergement</h3>
+                <p>GitHub</p>
+                <p>Adresse web : https://github.com/</p>
+            </article>
+            <article className="bloc-text">
+                <h3>Crédits</h3>
+                <p>Site internet réalisé par des Wilders</p>
+            </article>
+        </section>
+        <section className="bloc-infos">
+            <h2>Politique de confidentialité</h2>
+            <article className="bloc-text">
+                <h3>Qui sommes-nous ?</h3>
+                <p>Wildify est un site permettant à ses utilisateurs de faire des recherches de musiques, par styles, par artistes, etc. Il met également à disposition de ces derniers la possibilité de créer des playlistes à l’aide d’une librairie. Une page profil permet également une personnalisation des services.</p>
+            </article>
+            <article className="bloc-text">
+                <h3>Utilisation et conservation des données collectées</h3>
+                <p>Depuis le 25 mai 2018, le nouveau Règlement Général relatif à la Protection des Données fait évoluer la Loi dite « Informatique et Liberté ». Ce règlement encadre l’utilisation et la sécurisation de données à caractère personnel et vise à en assurer une meilleure protection, en octroyant le droit au client d’obtenir des informations sur les données récoltés par Wildify, demander à les modifier, les corriger, les restreindre ou à les supprimer (à noter que des dispositions légales nous imposent de conserver vos données).
+                    Ces dernières peuvent collectées lors de nos échanges ou de votre navigation sur nos pages. Ces données seront conservées durant 5 ans.
+                    Le client peut toutefois exercer ses droits en envoyant un courrier auprès de nos services.
+                </p>
+            </article>
+        </section>
+    </div>
+)
+
+export default Legal;
