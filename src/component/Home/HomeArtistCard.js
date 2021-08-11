c471354423e90ca19e43c5c9952b583ff2f96bb1
Maxime Jezequel <maxime.jezequel@gmail.com>
2021-08-11 18:56:14 +0200

    artists pictures added to home

--- src/component/Home/HomeArtistCard.js
+++ src/component/Home/HomeArtistCard.js
@@ -1,12 +1,33 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+import imgNotFound from '../../assets/cover-wildify.png'
 
 const HomeArtistCard = ({topArtist}) => {
+
+    // Reponse Call API TheAudioDb pour img Artist
+    const [img, setImg] = useState(null)
+
+    // Call API TheAudioDb pour img Artist
+    useEffect(() => {
+    const getImgAudioDb = () => {
+        axios.get(
+        `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${topArtist.name}`
+        ).then((results) => {
+            results.data.artists ? setImg(results.data.artists[0].strArtistThumb)
+            : setImg(imgNotFound)
+        })
+    }
+    topArtist && getImgAudioDb()
+    }, [topArtist])
 
     return (
         <div className = "homeTopCard" >
             <Link to={`/artist/${topArtist.name}`}> 
                 <p>{topArtist.name}</p>
-                <img src={topArtist.image[3]['#text']} alt="top artists"/>
+                <img src={img} alt={`Img of ${topArtist.name}`} />
+                {/* <img src={topArtist.image[3]['#text']} alt="top artists"/> */}
             </Link>
         </div>
     );
